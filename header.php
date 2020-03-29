6146a3aa80eaad9d4d2753bf9c66471c8aa9ffd4
ZySpec Development <zyspec@users.noreply.github.com>
2020-03-29 14:48:25 -0500

    minor updates to include '\' for namespace
    Also changed check for Theme to use 'instanceof'

--- header.php
+++ header.php
@@ -11,24 +11,21 @@ require XOOPS_ROOT_PATH . '/header.php';
 
 $moduleDirName = basename(__DIR__);
 
-$helper = \XoopsModules\Smallworld\Helper::getInstance();
+$helper = Smallworld\Helper::getInstance();
 
 $modulePath = XOOPS_ROOT_PATH . '/modules/' . $moduleDirName;
 
 $myts = \MyTextSanitizer::getInstance();
 
-if (!isset($GLOBALS['xoTheme']) || !is_object($GLOBALS['xoTheme'])) {
+if(!$GLOBALS['xoopsTpl'] instanceof \xos_opal_Theme) {
     require $GLOBALS['xoops']->path('class/theme.php');
     $GLOBALS['xoTheme'] = new \xos_opal_Theme();
 }
 
-//Handlers
-//$XXXHandler = xoops_getModuleHandler('XXX', $moduleDirName);
-
 // Load language files
 $helper->loadLanguage('main');
 
-if (!isset($GLOBALS['xoopsTpl']) || !($GLOBALS['xoopsTpl'] instanceof XoopsTpl)) {
+if (!$GLOBALS['xoopsTpl'] instanceof \XoopsTpl) {
     require $GLOBALS['xoops']->path('class/template.php');
-    $xoopsTpl = new XoopsTpl();
+    $GLOBALS['xoopsTpl'] = new \XoopsTpl();
 }
