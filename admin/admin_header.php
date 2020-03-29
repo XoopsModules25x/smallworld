35592bb8100c0bb5e325a5d0d0586c284d6881b4
ZySpec Development <zyspec@users.noreply.github.com>
2020-03-29 14:01:43 -0500

    fix redirect if user isn't a module admin
    previously code used $header->redirect() to attempt to send to absolute URL instead of relative to module
    
    - minor cleanup up phpDoc comment
    - removed duplicate definitions/instantiations that are already being done via ./include/common.php

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -14,46 +14,30 @@
  *
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
+ * @package      \XoopsModules\SmallWorld
  * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
  */
 
 use XoopsModules\Smallworld;
 
-$moduleDirName = basename(dirname(__DIR__));
-require_once __DIR__ . '/../../../include/cp_header.php';
-require_once __DIR__ . '/../include/common.php';
+require_once dirname(dirname(dirname(__DIR__))) . '/include/cp_header.php';
+require_once dirname(__DIR__) . '/include/common.php';
 
-require_once __DIR__ . '/upgrade.php';
+$db = \XoopsDatabaseFactory::getDatabase();
 
-// Check upgrade files
-smallworld_doUpgrade();
-
-$myts = \MyTextSanitizer::getInstance();
-$db   = \XoopsDatabaseFactory::getDatabase();
-
-//if (($GLOBALS['xoopsUser'] instanceof \XoopsUser)) {
-if ($GLOBALS['xoopsUser'] instanceof \XoopsUser) {
-    if (!$helper->isUserAdmin()) {
-        $helper->redirect(XOOPS_URL . '/', 3, _NOPERM);
-    }
-} else {
-    $helper->redirect(XOOPS_URL . '/user.php', 1, _NOPERM);
+if (!$helper->isUserAdmin()) {
+    redirect_header((XOOPS_URL . '/', 3, _NOPERM);
 }
 
 /** @var Xmf\Module\Admin $adminObject */
 $adminObject = \Xmf\Module\Admin::getInstance();
 
-if (!isset($GLOBALS['xoopsTpl']) || !($GLOBALS['xoopsTpl'] instanceof \XoopsTpl)) {
+if (!$GLOBALS['xoopsTpl'] instanceof \XoopsTpl) {
     require_once $GLOBALS['xoops']->path('class/template.php');
     $xoopsTpl = new \XoopsTpl();
 }
-
-$pathIcon16    = \Xmf\Module\Admin::iconUrl('', 16);
-$pathIcon32    = \Xmf\Module\Admin::iconUrl('', 32);
-$pathModIcon32 = $helper->getModule()->getInfo('modicons32');
 
 // Load language files
 $helper->loadLanguage('admin');
