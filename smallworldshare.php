765c978786b51d79c819cf591a70778d82f891f6
ZySpec Development <zyspec@users.noreply.github.com>
2020-03-29 17:30:36 -0500

    use Request to sanitize input vars
    * minor code cleanup
    * additional use of Helper class
    * minor phpDoc changes

--- smallworldshare.php
+++ smallworldshare.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -14,8 +14,8 @@
  *
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
+ * @package      \XoopsModules\SmallWorld
  * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
  */
@@ -25,26 +25,25 @@ use XoopsModules\Smallworld;
 
 require_once __DIR__ . '/header.php';
 
-require_once __DIR__ . '/../../mainfile.php';
 $GLOBALS['xoopsOption']['template_main'] = 'smallworld_share.html';
 require_once XOOPS_ROOT_PATH . '/header.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/functions.php';
-//require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/class_collector.php';
-global $xoopsUser, $xoTheme, $xoopsConfig, $xoopsTpl, $xoopsLogger;
 
-$xoopsLogger->activated = false;
+/** @var \XoopsModules\Smallworld\Helper $helper */
+require_once $helper->path('include/functions.php');
+
+$GLOBALS['xoopsLogger']->activated = false;
 /*error_reporting(E_ALL);*/
 
-if (isset($_GET['updid']) && isset($_GET['ownerid'])) {
-    $updID   = $_GET['updid'];
-    $ownerID = $_GET['ownerid'];
+if (Request::hasVar('updid', 'GET') && Request::hasVar('ownerid', 'GET')) {
+    $updID   = Request::getInt('updid', 0, 'GET');
+    $ownerID = Request::getInt('ownerid', 0, 'GET');
     $wm      = [];
 } else {
-    redirect_header(XOOPS_URL . '/modules/smallworld/index.php', 5, _SMALLWORLD_UPDATEID_NOT_EXIST);
+    $helper->redirect('index.php', 3, _SMALLWORLD_UPDATEID_NOT_EXIST);
 }
 
-$perm              = smallworldCheckPriv($updID);
-$xoopsTpl->caching = 0;
+$perm                         = smallworldCheckPriv($updID);
+$GLOBALS['xoopsTpl']->caching = 0;
 
 if ($perm <= 0) {
     // Things to do with wall
@@ -62,30 +61,30 @@ if ($perm <= 0) {
         $wm['priv']       = $data['priv'];
         $wm['avatar']     = $Wall->Gravatar($data['uid_fk']);
         if (smallworld_GetModuleOption('smallworldbookmarkavatar', $repmodule = 'smallworld' !== 1)) {
-            $wm['avatar_link'] = XOOPS_URL . '/modules/smallworld/assets/images/smallworld.png';
+            $wm['avatar_link'] = $helper->url('assets/images/smallworld.png');
         } else {
             $wm['avatar_link'] = smallworld_getAvatarLink($data['uid_fk'], $wm['avatar']);
         }
         $wm['avatar_size']     = smallworld_getImageSize(80, 100, $wm['avatar_link']);
         $wm['avatar_highwide'] = smallworld_imageResize($wm['avatar_size'][0], $wm['avatar_size'][1], 50);
-        $wm['usernameTitle']   = $wm['username'] . _SMALLWORLD_UPDATEONSITEMETA . $xoopsConfig['sitename'];
+        $wm['usernameTitle']   = $wm['username'] . _SMALLWORLD_UPDATEONSITEMETA . $GLOBALS['xoopsConfig']['sitename'];
         $desc                  = str_replace('UPLIMAGE', '', html_entity_decode($data['message'], ENT_QUOTES));
-        $xoopsTpl->assign('xoops_pagetitle', $wm['usernameTitle']);
-        if (isset($xoTheme) && is_object($xoTheme)) {
-            $xoTheme->addMeta('meta', 'description', $desc);
-            $xoTheme->addMeta('meta', 'title', $wm['usernameTitle']);
-            $xoTheme->addMeta('meta', 'author', $wm['username']);
-            $xoTheme->addMeta('meta', 'og:title', $wm['usernameTitle']);
-            $xoTheme->addMeta('meta', 'og:url', XOOPS_URL . '/modules/smallworld/index.php');
-            $xoTheme->addMeta('meta', 'og:site_name', $xoopsConfig['sitename']);
-            $xoTheme->addMeta('meta', 'og:description', $desc);
-            $xoTheme->addMeta('meta', 'og:image', $wm['avatar_link']);
-            $xoTheme->addMeta('meta', 'og:updated_time', $data['created']);
+        $GLOBALS['xoopsTpl']->assign('xoops_pagetitle', $wm['usernameTitle']);
+        if ($GLOBALS['xoTheme'] instanceof \xos_opal_Theme) {
+            $GLOBALS['xoTheme']->addMeta('meta', 'description', $desc);
+            $GLOBALS['xoTheme']->addMeta('meta', 'title', $wm['usernameTitle']);
+            $GLOBALS['xoTheme']->addMeta('meta', 'author', $wm['username']);
+            $GLOBALS['xoTheme']->addMeta('meta', 'og:title', $wm['usernameTitle']);
+            $GLOBALS['xoTheme']->addMeta('meta', 'og:url', $helper->url('index.php'));
+            $GLOBALS['xoTheme']->addMeta('meta', 'og:site_name', $xoopsConfig['sitename']);
+            $GLOBALS['xoTheme']->addMeta('meta', 'og:description', $desc);
+            $GLOBALS['xoTheme']->addMeta('meta', 'og:image', $wm['avatar_link']);
+            $GLOBALS['xoTheme']->addMeta('meta', 'og:updated_time', $data['created']);
         }
-        $xoopsTpl->append('walldata', $wm);
+        $GLOBALS['xoopsTpl']->append('walldata', $wm);
     }
 } else {
-    $xoTheme->addMeta('meta', 'og:image', XOOPS_URL . '/modules/smallworld/assets/images/smallworld.png');
+    $GLOBALS['xoTheme']->addMeta('meta', 'og:image', $helper->url('assets/images/smallworld.png'));
 }
 
 require_once XOOPS_ROOT_PATH . '/footer.php';
