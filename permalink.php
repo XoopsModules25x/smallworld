6fdb1669d7acd6fb76d3752340386128c4f09995
zyspec <zyspec@yahoo.com>
2020-04-15 18:45:37 -0500

    minor update to permalink.pph
    * use $swDB instead of $dB for SwDatabase to reduce confusion
    * use Constants interface constants
    * use Smallworld\Helper where appropriate
    * combine tpl assigns to improve performance
    * update phpDoc comments

--- permalink.php
+++ permalink.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -12,77 +12,76 @@
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\Smallworld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
  */
 
+use Xmf\Request;
 use XoopsModules\Smallworld;
+use XoopsModules\Smallworld\Constants;
 
 require_once __DIR__ . '/header.php';
 
 $GLOBALS['xoopsOption']['template_main'] = 'smallworld_permalink.tpl';
 require_once XOOPS_ROOT_PATH . '/header.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/functions.php';
-//require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/class_collector.php';
-global $xoopsUser, $xoTheme, $xoopsLogger, $xoopsModule;
 
-$xoopsLogger->activated = false;
+/** @var \XoopsModules\Smallworld\Helper $helper */
+require_once $helper->path('include/functions.php');
+
+$GLOBALS['xoopsLogger']->activated = false;
 /* error_reporting(E_ALL); */
 
-$moduleHandler = xoops_getHandler('module');
-$module        = $moduleHandler->getByDirname('smallworld');
-$configHandler = xoops_getHandler('config');
-$moduleConfig  = $configHandler->getConfigsByCat(0, $module->getVar('mid'));
-
-$set   = smallworld_checkPrivateOrPublic();
+//$set   = smallworld_checkPrivateOrPublic();
 $check = new Smallworld\User();
-$pub   = smallworld_checkUserPubPostPerm();
-$dBase = new Smallworld\SwDatabase();
+//$pub   = smallworld_checkUserPubPostPerm();
 
-if (isset($_GET['updid']) and isset($_GET['ownerid'])) {
-    $updID   = $_GET['updid'];
-    $ownerID = $_GET['ownerid'];
+if (Request::hasVar('updid', 'GET') && Request::hasVar('ownerid', 'GET')) {
+    $updID   = Request::getInt('updid', 0, 'GET');
+    $ownerID = Request::getInt('ownerid', 0, 'GET');
 } else {
-    redirect_header(XOOPS_URL . '/modules/smallworld/index.php', 5, _SMALLWORLD_UPDATEID_NOT_EXIST);
+    $helper->redirect('index.php', Constants::REDIRECT_DELAY_MEDIUM, _SMALLWORLD_UPDATEID_NOT_EXIST);
 }
 
-$id   = $xoopsUser ? $xoopsUser->getVar('uid') : 0;
-$user = $xoopsUser ? new \XoopsUser($id) : 0;
-
-if ($xoopsUser) {
-    if ($xoopsUser->isAdmin($xoopsModule->getVar('mid'))) {
+$id = 0;
+$is_admin   = $helper->userIsAdmin() ? 'YES' : 'NO';
+$username   = '';
+if ($GLOBALS['xoopsUser'] && $GLOBALS['xoopsUser'] instanceof \XoopsUser) {
+    $id       = $GLOBALS['xoopsUser']->uid();
+    $username = $GLOBALS['xoopsUser']->uname();
+    /** {@internal not used}}
+    if ($helper->userIsAdmin()) {
         $pub = $check->allUsers();
-        $xoopsTpl->assign('isadminuser', 'YES');
-        $xoopsTpl->assign('ownerofpage', $id);
-    } else {
-        $xoopsTpl->assign('isadminuser', 'NO');
+        $GLOBALS['xoopsTpl']->assign('ownerofpage', $id); - not used
     }
+    */
 }
-$username = $xoopsUser ? $user->getVar('uname') : '';
 
-$friend = $check->friendcheck($id, $ownerID);
-
+//$friend         = $check->friendcheck($id, $ownerID);
 $profile        = $check->checkIfProfile($id);
-$menu_startpage = "<a href='" . XOOPS_URL . "/modules/smallworld/publicindex.php'><img id='menuimg' src='" . XOOPS_URL . "/modules/smallworld/assets/images/highrise.png'>" . _SMALLWORLD_STARTPAGE . '</a>';
-$menu_home      = "<a href='" . XOOPS_URL . "/modules/smallworld/'><img id='menuimg' src='" . XOOPS_URL . "/modules/smallworld/assets/images/house.png'>" . _SMALLWORLD_HOME . '</a>';
+$menu_startpage = "<a href='" . $helper->url('publicindex.php') . "'><img id='menuimg' src='" . $helper->url('assets/images/highrise.png') . "'>" . _SMALLWORLD_STARTPAGE . '</a>';
+$menu_home      = "<a href='" . $helper->url('index.php') . "'><img id='menuimg' src='" . $helper->url('assets/images/house.png') . "'>" . _SMALLWORLD_HOME . '</a>';
 
 // Things to do with wall
 $wall = new Smallworld\WallUpdates();
 
-// Follow array here
-$followers = smallworld_array_flatten($wall->getFollowers($id), 0);
+// Follow array here - not used
+//$followers = smallworld_array_flatten($wall->getFollowers($id), 0);
 
 $updatesarray = $wall->updatesPermalink($updID, $id, $ownerID);
 $wall->parsePubArray($updatesarray, $id);
 
-$xoopsTpl->assign('menu_startpage', $menu_startpage);
-$xoopsTpl->assign('menu_home', $menu_home);
-$xoopsTpl->assign('myusername', $username);
-$xoopsTpl->assign('pagename', 'index');
-$xoopsTpl->assign('check', $profile);
+$GLOBALS['xoopsTpl']->assign([
+    'menu_startpage' => $menu_startpage,
+    'menu_home'      => $menu_home,
+    'myusername'     => $username,
+    'pagename'       => 'index',
+    'check'          => $profile,
+    'isadminuser'    => $is_admin
+]);
 
 require_once XOOPS_ROOT_PATH . '/footer.php';
