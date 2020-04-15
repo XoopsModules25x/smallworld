84e51fe486e7f653d9acabff6377e7824f2abd17
zyspec <zyspec@yahoo.com>
2020-04-15 18:27:54 -0500

    minor updates
    * switch from global to $GLOBALS
    * sanitize incoming vars using Xmf\Request
    * combine tpl assigns to improve performance
    * use Constants interface constants
    * use Smallworld\Helper where appropriate
    * update phpDoc comments

--- editimages.php
+++ editimages.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -12,54 +12,53 @@
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\Smallworld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
  */
 
 use XoopsModules\Smallworld;
+use XoopsModules\Smallworld\Constants;
 
 require_once __DIR__ . '/header.php';
 
 $GLOBALS['xoopsOption']['template_main'] = 'smallworld_images_edittemplate.tpl';
 require_once XOOPS_ROOT_PATH . '/header.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/functions.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/arrays.php';
-//require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/class_collector.php';
-global $xoopsUser, $xoopsTpl, $xoopsDB, $xoTheme;
+/** @var \XoopsModules\Smallworld\Helper $helper */
+require_once $helper->path('include/functions.php');
+require_once $helper->path('include/arrays.php');
 
-if ($xoopsUser) {
-    $userID = $xoopsUser->getVar('uid');
+if ($GLOBALS['xoopsUser'] && ($GLOBALS['xoopsUser'] instanceof \XoopsUser)) {
+    $userID = $GLOBALS['xoopsUser']->getVar('uid');
 
     // Check if inspected userid -> redirect to userprofile and show admin countdown
     $inspect = smallworld_isInspected($userID);
     if ('yes' === $inspect['inspect']) {
-        redirect_header('userprofile.php?username=' . $xoopsUser->getVar('uname'), 1);
+        $helper->redirect('userprofile.php?username=' . $GLOBALS['xoopsUser']->uname());
     }
 
     $check   = new Smallworld\User();
     $profile = $check->checkIfProfile($userID);
-    if ($profile > 0) {
-        $xoopsTpl->assign('check', $profile);
-        $item = new Smallworld\Form();
-        $db   = new Smallworld\SwDatabase();
-
+    if (Constants::PROFILE_HAS_BOTH <= $profile) {
+        $GLOBALS['xoopsTpl']->assign('check', $profile);
+        $item       = new Smallworld\Form();
         $editimages = '';
-        $sql        = 'SELECT *FROM ' . $xoopsDB->prefix('smallworld_images') . " WHERE userid = '" . $userID . "'";
-        $result     = $xoopsDB->query($sql);
+        $sql        = 'SELECT *FROM ' . $GLOBALS['xoopsDB']->prefix('smallworld_images') . " WHERE userid = '" . $userID . "'";
+        $result     = $GLOBALS['xoopsDB']->query($sql);
         $i          = 0;
-        while (false !== ($sqlfetch = $xoopsDB->fetchArray($result))) {
+        while (false !== ($sqlfetch = $GLOBALS['xoopsDB']->fetchArray($result))) {
             $editimages .= $item->edit_images($userID, $sqlfetch['imgurl'], $sqlfetch['desc'], $sqlfetch['id']);
             ++$i;
         }
-        $xoopsTpl->append('editimages', $editimages);
-        $xoTheme->addScript(XOOPS_URL . '/modules/smallworld/assets/js/jquery.colorbox.js');
-        //$xoTheme->addStylesheet(XOOPS_URL.'/modules/smallworld/assets/css/colorbox.css');
+        $GLOBALS['xoopsTpl']->append('editimages', $editimages);
+        $GLOBALS['xoTheme']->addScript($helper->url('assets/js/jquery.colorbox.js'));
+        //$GLOBALS['xoTheme']->addStylesheet($helper->url('assets/css/colorbox.css'));
     } else {
-        redirect_header('index.php', 1);
+        redirect_header('index.php', Constants::REDIRECT_DELAY_SHORT);
     }
 }
 require_once XOOPS_ROOT_PATH . '/footer.php';
--- galleryshow.php
+++ galleryshow.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -12,69 +12,71 @@
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\Smallworld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
  */
 
+use Xmf\Request;
 use XoopsModules\Smallworld;
+use XoopsModules\Smallworld\Constants;
 
 require_once __DIR__ . '/header.php';
 
-$GLOBALS['xoopsOption']['template_main'] = 'smallworld_galleryshow.html';
+$GLOBALS['xoopsOption']['template_main'] = 'smallworld_galleryshow.tpl';
 require_once XOOPS_ROOT_PATH . '/header.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/functions.php';
-//require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/class_collector.php';
-global $xoopsUser, $xoTheme, $xoopsLogger;
-$xoopsLogger->activated = false;
 
-$admin = false;
+/** @var \XoopsModules\Smallworld\Helper $helper */
+require_once $helper->path('include/functions.php');
 
-if ($xoopsUser) {
-    $id           = $xoopsUser->getVar('uid');
+$GLOBALS['xoopsLogger']->activated = false;
+
+$admin = $helper->isUserAdmin() ? true: false;
+
+if ($GLOBALS['xoopsUser'] && ($GLOBALS['xoopsUser'] instanceof \XoopsUser)) {
+    $id           = $GLOBALS['xoopsUser']->uid();
     $check        = new Smallworld\User();
     $image        = new Smallworld\Images();
-    $username     = $_GET['username'];
+    $username     = Request::getString('username', '', 'GET');
     $userID       = smallworld_isset_or($_GET['username']); // Id of user wich profile you want to see
     $userisfriend = $check->friendcheck($id, $userID);
 
-    if ($xoopsUser->isAdmin($xoopsModule->getVar('mid'))) {
-        $xoopsTpl->assign('isadminuser', 'YES');
-        $admin = true;
-    }
+    $tpl_admin = $admin ? 'YES' : 'NO';
+    $GLOBALS['xoopsTpl']->assign('isadminuser', $tpl_admin);
 
     // Check if inspected userid -> redirect to userprofile and show admin countdown
     $inspect = smallworld_isInspected($id);
     if ('yes' === $inspect['inspect']) {
-        redirect_header('userprofile.php?username=' . $xoopsUser->getVar('uname'), 1, _SMALLWORLD_INSPEC_usermsg);
+        $helper->redirect('userprofile.php?username=' . $GLOBALS['xoopsUser']->uname(), Constants::REDIRECT_DELAY_SHORT, _SMALLWORLD_INSPEC_usermsg);
     }
 
     $profile = $check->checkIfProfile($id);
-    if ($profile >= 2 || 2 == $userisfriend[0] || true === $admin) {
-        $myusername = $xoopsUser->getVar('uname');
-
-        $user        = new \XoopsUser($id);
+    if ($profile >= Constants::PROFILE_HAS_BOTH || 2 == $userisfriend[0] || true === $admin) {
+        $myusername  = $GLOBALS['xoopsUser']->uname();
         $countimages = $image->count($userID);
 
-        //$gallery = $image->viewalbum ($id, $user=$xoopsUser->getVar('uid'));
+        //$gallery = $image->viewalbum ($id, $user=$GLOBALS['xoopsUser']->getVar('uid'));
         $gallery = $image->viewalbum($id, $userID);
-        $xoopsTpl->assign('countimages', $countimages);
-        $xoopsTpl->assign('userisfriend', $userisfriend[0]);
-        $xoopsTpl->assign('gallery', $gallery);
-        $xoopsTpl->assign('closealbum', _SMALLWORLD_ALBUMTITLETEXT);
-        $xoopsTpl->assign('username', $username);
-        $xoopsTpl->assign('myusername', $myusername);
-        $xoopsTpl->assign('gallerytitleheader', _SMALLWORLD_TITLEHEADER);
-        $xoopsTpl->assign('check', 1);
-    } elseif (0 == $profile) {
+        $GLOBALS['xoopsTpl']->assign([
+            'countimages'        => $countimages,
+            'userisfriend'       => $userisfriend[0],
+            'gallery'            => $gallery,
+            'closealbum'         => _SMALLWORLD_ALBUMTITLETEXT,
+            'username'           => $username,
+            'myusername'         => $myusername,
+            'gallerytitleheader' => _SMALLWORLD_TITLEHEADER,
+            'check'              => 1
+        ]);
+    } elseif (Constants::PROFILE_NONE == $profile) {
         $check->chkUser();
     } else {
-        redirect_header('userprofile.php?username=' . $xoopsUser->getVar('uname'), 1, _NOPERM);
+        $helper->redirect('userprofile.php?username=' . $GLOBALS['xoopsUser']->uname(), Constants::REDIRECT_DELAY_SHORT, _NOPERM);
     }
 } else {
-    redirect_header(XOOPS_URL . '/user.php', 1, _NOPERM);
+    redirect_header(XOOPS_URL . '/user.php', Constants::REDIRECT_DELAY_SHORT, _NOPERM);
 }
 require_once XOOPS_ROOT_PATH . '/footer.php';
