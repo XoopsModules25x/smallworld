276a988e973f1c9a6f30400b45c71b0c460f9ca9
zyspec <zyspec@yahoo.com>
2020-04-15 17:26:37 -0500

    add missing files after rename

--- include/get_selects.php
+++ include/get_selects.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -12,34 +12,45 @@
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\Smallworld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
  */
 
 use XoopsModules\Smallworld;
 
-require_once __DIR__ . '/../../../mainfile.php';
+require_once dirname(dirname(dirname(__DIR__))) . ('/mainfile.php');
 require_once XOOPS_ROOT_PATH . '/header.php';
 require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/functions.php';
 require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/arrays.php';
-//require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/class_collector.php';
-global $xoopsUser, $xoopsModule, $xoopsLogger;
-$xoopsLogger->activated = false;
-if ($xoopsUser) {
-    $db       = new Smallworld\SwDatabase();
-    $id       = $xoopsUser->getVar('uid');
-    $relat    = $db->getVar($id, 'relationship');
-    $partner  = $db->getVar($id, 'partner');
-    $gender   = $db->getVar($id, 'gender');
-    $politic  = $db->getVar($id, 'politic');
-    $religion = $db->getVar($id, 'religion');
+
+$GLOBALS['xoopsLogger']->activated = false;
+//$userString = '{"relat":"", "partner":"", "gender":"", "politic":"", "religion":""}';
+$userString = '{"relat":"0", "partner":"", "gender":"0", "politic":"0", "religion":"0"}';
+
+if ($GLOBALS['xoopsUser'] && ($GLOBALS['xoopsUser'] instanceof \XoopsUser)) {
+    $swUserHandler = Smallworld\Helper::getInstance()->getHandler('SwUser');
+    $user          = $swUserHandler->get($GLOBALS['xoopsUser']->uid()); // get the user
+    if ($user instanceof \XoopsModules\Smallworld\SwUser) {
+        $userVals      = $user->getValues();
+        $userString = "{\"relat\":\"{$userVals['relationship']}\", \"partner\":\"{$userVals['partner']}\", \"gender\":\"{$userVals['gender']}\", \"politic\":\"{$userVals['politic']}\", \"religion\":\"{$userVals['religion']}\"}";
+    }
+/*
+    $swDB     = new Smallworld\SwDatabase();
+    $id       = $GLOBALS['xoopsUser']->uid();
+    $relat    = $swDB->getVar($id, 'relationship');
+    $partner  = $swDB->getVar($id, 'partner');
+    $gender   = $swDB->getVar($id, 'gender');
+    $politic  = $swDB->getVar($id, 'politic');
+    $religion = $swDB->getVar($id, 'religion');
     header('Content-type: "application/json"');
-    echo "{\"relat\":\"$relat\", \"partner\":\"$partner\", \"gender\":\"$gender\", \"politic\":\"$politic\", \"religion\":\"$religion\"}";
-} else {
-    header('Content-type: "application/json"');
-    echo '{"relat":"", "partner":"", "gender":"", "politic":"", "religion":""}';
+    echo "{\"relat\":\"{$relat}\", \"partner\":\"{$partner}\", \"gender\":\"{$gender}\", \"politic\":\"{$politic}\", \"religion\":\"{$religion}\"}";
+*/
 }
+
+header('Content-type: "application/json"');
+echo $userString;
