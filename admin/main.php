cba80d4ad286a28dc8613c3bd0f9a7ee428b347a
zyspec <zyspec@yahoo.com>
2020-04-15 20:56:58 -0500

    bug fix and update of ./admin/main.php
    * fix bug loading tpl as .html instead of .tpl
    * fix wrong name bug looking for css file
    * fix bugs not looking for js scripts in ./assets/js folder
    * fix bug looking for jquery.ui.datepicker-language.js file
    * fix bug looking for variables.js language file
    * use Smallworld\Helper where appropriate
    * switch from global to $GLOBALS
    * combine tpl assigns to improve performance
    * use Constants interface constants
    * update phpDoc comments

--- admin/main.php
+++ admin/main.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -12,66 +12,83 @@
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\SmallWorld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
  */
 
+use Xmf\Request;
 use XoopsModules\Smallworld;
+use XoopsModules\Smallworld\Constants;
 
 require_once __DIR__ . '/admin_header.php';
-
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/functions.php';
-//require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/class_collector.php';
-
+/**
+ * Vars defined by inclusion of ./admin_header.php
+ *
+ * @var \XoopsModules\Smallworld\Admin $admin
+ * @var \XoopsModuels\Smallworld\SwUserHandler $swUserHandler
+ * @var \XoopsModules\Smallworld\DoSync $d
+ * @var \XoopsModules\Smallworld\User $check
+ * @var \XoopsModules\Smallworld\SwDatabase $swDB
+ * @var \XoopsModules\Smallworld\WallUpdates $wall
+ * @var \Xmf\Module\Admin $adminObject
+ * @var \XoopsModules\Smallworld\Helper $helper
+ * @var string $moduleDirName
+ * @var string $moduleDirNameUpper
+ */
+require_once $helper->path('include/functions.php');
 require_once XOOPS_ROOT_PATH . '/class/template.php';
-if (!isset($xoopsTpl)) {
-    $xoopsTpl = new \XoopsTpl();
-}
-$xoopsTpl->caching = 0;
+$GLOBALS['xoopsTpl']->caching = 0;
 
 xoops_cp_header();
 
-if (isset($_POST['xim_admin_message'])) {
+if (Request::hasVar('xim_admin_message', 'POST')) {
     $_POST['xim_admin_message'] = '';
 }
 
 $admin = new Smallworld\Admin();
-$tpl   = new \XoopsTpl();
 
 $ai  = [];
 $ani = [];
 
 // --------------- First tab in admin ---------------
 // Find oldest message and apply to template
-$dateoffirstmessage = date('d-m-Y H:i:s', $admin->oldestMsg());
+
+//$dateoffirstmessage = date('d-m-Y H:i:s', $admin->oldestMsg());
+$dfm = $admin->oldestMsg();
+$dateoffirstmessage = (Constants::NO_DATE == $dfm) ? _AM_SMALLWORLD_NONEYET : date(_SHORTDATESTRING, $dfm);
+
 // Get days number
 $totaldays = $admin->countDays();
 // get average messages per day
 $avgperday = $admin->AvgMsgDay($totaldays);
-// XIM version number
+// Smallworld version number
 $installversion = $admin->ModuleInstallVersion();
-// XIM install date
+// Smallworld install date
 $installdate = $admin->ModuleInstallDate();
 
 //check current version of XIM, return desc,link,version if new available
 $installCheck = $admin->doCheckUpdate();
 
 // Count members using XIM
-$sumallusers = $admin->TotalUsers();
+$sumallusers = $swUserHandler->getCount();
+//$sumallusers = $admin->TotalUsers();
+
 // Find list of most active users (total)
-$admin->mostactiveusers_allround();
+$GLOBALS['xoopsTpl']->assign('topuser', $admin->mostactiveusers_allround());
+
 // Find list of most active users (24 hours)
-$admin->mostactiveusers_today();
+$GLOBALS['xoopsTpl']->assign('topusertoday', $admin->mostactiveusers_today());
 
-// FInd list of best rated users overall
-$admin->topratedusers('up');
+// Find list of best rated users overall
+$GLOBALS['xoopsTpl']->assign('topratedusers', $admin->topratedusers('up'));
 
-// FInd list of worst rated users overall
-$admin->topratedusers('down');
+// Find list of worst rated users overall
+$GLOBALS['xoopsTpl']->assign('bottomratedusers', $admin->topratedusers('down'));
 
 $allusers_inspect = $admin->getAllUsers('yes');
 if (!empty($allusers_inspect)) {
@@ -88,10 +105,10 @@ if (!empty($allusers_inspect)) {
         $ai['inspect_start']         = $data['inspect_start'];
         $ai['inspect_stop']          = $data['inspect_stop'];
         $ai['userinspect_timetotal'] = ($data['inspect_start'] + $data['inspect_stop']) - time();
-        $xoopsTpl->append('allusersinspect', $ai);
+        $GLOBALS['xoopsTpl']->append('allusersinspect', $ai);
     }
 }
-$xoopsTpl->assign('allusersinspectcounter', count($ai));
+$GLOBALS['xoopsTpl']->assign('allusersinspectcounter', count($ai));
 
 $allusers_noinspect = $admin->getAllUsers('no');
 if (!empty($allusers_noinspect)) {
@@ -108,86 +125,89 @@ if (!empty($allusers_noinspect)) {
         $ani['inspect_start']         = '';
         $ani['inspect_stop']          = '';
         $ani['userinspect_timetotal'] = '';
-        $xoopsTpl->append('allusersnoinspect', $ani);
+        $GLOBALS['xoopsTpl']->append('allusersnoinspect', $ani);
     }
 }
-$xoopsTpl->assign('allusersnoinspectcounter', count($ani));
+$GLOBALS['xoopsTpl']->assign('allusersnoinspectcounter', count($ani));
 // ---------------- end of tabs ---------------- //
 
 // template assignments
 // tab titles
-$xoopsTpl->assign('lang_statistics', _AM_SMALLWORLD_STATISTICS_TITLE);
-$xoopsTpl->assign('lang_moduleinfo', _AM_SMALLWORLD_MODULEINFO);
-$xoopsTpl->assign('lang_userstats', _AM_SMALLWORLD_USERSTATS);
-$xoopsTpl->assign('lang_installversion', _AM_SMALLWORLD_MODULEINSTALL);
-$xoopsTpl->assign('lang_installversion_status', _AM_SMALLWORLD_UPDATE_STATUS);
-$xoopsTpl->assign('lang_installdate', _AM_SMALLWORLD_INSTALLDATE);
-$xoopsTpl->assign('lang_dateoffirstmessage', _AM_SMALLWORLD_DATEOFFIRSTMESSAGE);
-$xoopsTpl->assign('lang_totalusers', _AM_SMALLWORLD_TOTALUSERS);
-$xoopsTpl->assign('lang_averagemsgperday', _AM_SMALLWORLD_AVERAGEMSGPERDAY);
-$xoopsTpl->assign('lang_topchatters', _AM_SMALLWORLD_TOPCHATTERS);
-$xoopsTpl->assign('lang_topchatterstoday', _AM_SMALLWORLD_TOPCHATTERS_TODAY);
-$xoopsTpl->assign('lang_toprated', _AM_SMALLWORLD_TOPRATEDUSERS);
-$xoopsTpl->assign('lang_bottomrated', _AM_SMALLWORLD_BOTTOMRATEDUSERS);
-$xoopsTpl->assign('lang_useradmin', _AM_SMALLWORLD_USERADMIN_TITLE);
-$xoopsTpl->assign('lang_help', _AM_SMALLWORLD_HELP);
-$xoopsTpl->assign('lang_prefs', _MI_SYSTEM_ADMENU6);
-$xoopsTpl->assign('lang_prefslink', "<a href='../../system/admin.php?fct=preferences&amp;op=showmod&amp;mod=" . $xoopsModule->getVar('mid') . "'>" . _MI_SYSTEM_ADMENU6 . '</a>');
+$GLOBALS['xoopsTpl']->assign([
+    'lang_statistics'            => _AM_SMALLWORLD_STATISTICS_TITLE,
+    'lang_moduleinfo'            => _AM_SMALLWORLD_MODULEINFO,
+    'lang_userstats'             => _AM_SMALLWORLD_USERSTATS,
+    'lang_installversion'        => _AM_SMALLWORLD_MODULEINSTALL,
+    'lang_installversion_status' => _AM_SMALLWORLD_UPDATE_STATUS,
+    'lang_installdate'           => _AM_SMALLWORLD_INSTALLDATE,
+    'lang_dateoffirstmessage'    => _AM_SMALLWORLD_DATEOFFIRSTMESSAGE,
+    'lang_totalusers'            => _AM_SMALLWORLD_TOTALUSERS,
+    'lang_averagemsgperday'      => _AM_SMALLWORLD_AVERAGEMSGPERDAY,
+    'lang_topchatters'           => _AM_SMALLWORLD_TOPCHATTERS,
+    'lang_topchatterstoday'      => _AM_SMALLWORLD_TOPCHATTERS_TODAY,
+    'lang_toprated'              => _AM_SMALLWORLD_TOPRATEDUSERS,
+    'lang_bottomrated'           => _AM_SMALLWORLD_BOTTOMRATEDUSERS,
+    'lang_useradmin'             => _AM_SMALLWORLD_USERADMIN_TITLE,
+    'lang_help'                  => _AM_SMALLWORLD_HELP,
+    'lang_prefs'                 => _MI_SYSTEM_ADMENU6,
+    'lang_prefslink'             => "<a href='../../system/admin.php?fct=preferences&amp;op=showmod&amp;mod=" . $helper->getModule()->mid() . "'>" . _MI_SYSTEM_ADMENU6 . '</a>'
+]);
 
 // help file from admin
-$xoopsTpl->assign('lang_hlp_about', _AM_SMALLWORLD_HELP_ABOUT);
-$xoopsTpl->assign('lang_hlp_preface', _AM_SMALLWORLD_HELP_PREFACE);
-$xoopsTpl->assign('lang_hlp_requirements_t', _AM_SMALLWORLD_HELP_HEADER_REQUIREMENTS);
-$xoopsTpl->assign('lang_hlp_requirements', _AM_SMALLWORLD_HELP_REQUIREMENTS);
-$xoopsTpl->assign('lang_hlp_recommended_t', _AM_SMALLWORLD_HELP_HEADER_RECOMMENDED);
-$xoopsTpl->assign('lang_hlp_recommended', _AM_SMALLWORLD_HELP_RECOMMENDED);
-$xoopsTpl->assign('lang_hlp_installation_t', _AM_SMALLWORLD_HELP_HEADER_INSTALLATION);
-$xoopsTpl->assign('lang_hlp_firsttime', _AM_SMALLWORLD_HELP_FIRSTTIMEINSTALL);
-$xoopsTpl->assign('lang_hlp_hostedplatform_t', _AM_SMALLWORLD_HELP_HEADER_HOSTED_PLATFORM);
-$xoopsTpl->assign('lang_hlp_hostedplatform', _AM_SMALLWORLD_HELP_HOSTED_PLATFORM);
-$xoopsTpl->assign('lang_hlp_upgrading_t', _AM_SMALLWORLD_HELP_HEADER_UPGRADING);
-$xoopsTpl->assign('lang_hlp_upgrading', _AM_SMALLWORLD_HELP_UPGRADING);
-$xoopsTpl->assign('lang_hlp_faq_t', _AM_SMALLWORLD_HELP_HEADER_FAQ);
-$xoopsTpl->assign('lang_hlp_commen1_t', _AM_SMALLWORLD_HELP_HEADER_COMMENPROBLEMS1);
-$xoopsTpl->assign('lang_hlp_commen1', _AM_SMALLWORLD_HELP_COMMENPROBLEMS1);
-$xoopsTpl->assign('lang_hlp_contacts_t', _AM_SMALLWORLD_HELP_HEADER_CONTACTS);
-$xoopsTpl->assign('lang_hlp_otherhelp', _AM_SMALLWORLD_HELP_OTHERHELP);
-$xoopsTpl->assign('installversion', $installversion);
-$xoopsTpl->assign('installdate', $installdate);
-$xoopsTpl->assign('installversion_status', $installCheck);
-$xoopsTpl->assign('dateoffirstmessage', $dateoffirstmessage);
-$xoopsTpl->assign('totalusers', $sumallusers);
-$xoopsTpl->assign('averagemsgperday', $avgperday);
-$xoopsTpl->display('db:smallworld_admin.html');
+$GLOBALS['xoopsTpl']->assign([
+    'lang_hlp_about'            => _AM_SMALLWORLD_HELP_ABOUT,
+    'lang_hlp_preface'          => _AM_SMALLWORLD_HELP_PREFACE,
+    'lang_hlp_requirements_t'   => _AM_SMALLWORLD_HELP_HEADER_REQUIREMENTS,
+    'lang_hlp_requirements'     => _AM_SMALLWORLD_HELP_REQUIREMENTS,
+    'lang_hlp_recommended_t'    => _AM_SMALLWORLD_HELP_HEADER_RECOMMENDED,
+    'lang_hlp_recommended'      => _AM_SMALLWORLD_HELP_RECOMMENDED,
+    'lang_hlp_installation_t'   => _AM_SMALLWORLD_HELP_HEADER_INSTALLATION,
+    'lang_hlp_firsttime'        => _AM_SMALLWORLD_HELP_FIRSTTIMEINSTALL,
+    'lang_hlp_hostedplatform_t' => _AM_SMALLWORLD_HELP_HEADER_HOSTED_PLATFORM,
+    'lang_hlp_hostedplatform'   => _AM_SMALLWORLD_HELP_HOSTED_PLATFORM,
+    'lang_hlp_upgrading_t'      => _AM_SMALLWORLD_HELP_HEADER_UPGRADING,
+    'lang_hlp_upgrading'        => _AM_SMALLWORLD_HELP_UPGRADING,
+    'lang_hlp_faq_t'            => _AM_SMALLWORLD_HELP_HEADER_FAQ,
+    'lang_hlp_commen1_t'        => _AM_SMALLWORLD_HELP_HEADER_COMMENPROBLEMS1,
+    'lang_hlp_commen1'          => _AM_SMALLWORLD_HELP_COMMENPROBLEMS1,
+    'lang_hlp_contacts_t'       => _AM_SMALLWORLD_HELP_HEADER_CONTACTS,
+    'lang_hlp_otherhelp'        => _AM_SMALLWORLD_HELP_OTHERHELP,
+    'installversion'            => $installversion,
+    'installdate'               => $installdate,
+    'installversion_status'     => $installCheck,
+    'dateoffirstmessage'        => $dateoffirstmessage,
+    'totalusers'                => $sumallusers,
+    'averagemsgperday'          => $avgperday
+]);
+$GLOBALS['xoopsTpl']->display('db:smallworld_admin.tpl');
 
-global $xoTheme;
 //Check Language
-$lang = $xoopsConfig['language'];
+$lang = $GLOBALS['xoopsConfig']['language'];
 // GET various variables from language folder
-if (file_exists(XOOPS_ROOT_PATH . '/modules/smallworld/language/js/' . $lang . '/variables.js')) {
-    $xoTheme->addScript(XOOPS_URL . '/modules/smallworld/language/' . $lang . '/js/variables.js');
+if (file_exists($helper->path('language/' . $lang . '/js/variables.js'))) {
+    $GLOBALS['xoTheme']->addScript($helper->url('language/' . $lang . '/js/variables.js'));
 } else {
-    $xoTheme->addScript(XOOPS_URL . '/modules/smallworld/language/english/js/variables.js');
+    $GLOBALS['xoTheme']->addScript($helper->url('language/english/js/variables.js'));
 }
 
 $adminscript = <<<SCRIPT
         var smallworld_url="XOOPS_URL/modules/smallworld/";
-        var $ = jQuery();
+        //var $ = jQuery();
 SCRIPT;
-$xoTheme->addStylesheet('modules/smallworld/assets/css/Admin.css');
-$xoTheme->addScript(XOOPS_URL . '/browse.php?Frameworks/jquery/jquery.js');
-$xoTheme->addScript(XOOPS_URL . '/modules/smallworld/js/jquery-ui-1.8.11.custom.js');
-$xoTheme->addScript('modules/smallworld/js/smallworld_tabs.js');
-$xoTheme->addScript('', '', $adminscript);
-$xoTheme->addScript(XOOPS_URL . '/modules/smallworld/js/jquery.form.js');
-$xoTheme->addScript(XOOPS_URL . '/modules/smallworld/js/jquery.countdown.js');
-if (file_exists(XOOPS_ROOT_PATH . '/modules/smallworld/language/js/' . $lang . '/jquery.ui.datepicker-language.js')) {
-    $xoTheme->addScript(XOOPS_URL . '/modules/smallworld/language/' . $lang . '/js/jquery.ui.datepicker-language.js');
-    $xoTheme->addScript(XOOPS_URL . '/modules/smallworld/language/' . $lang . '/js/jquery.countdown.js');
+$GLOBALS['xoTheme']->addStylesheet($helper->url('assets/css/SmallworldAdmin.css'));
+$GLOBALS['xoTheme']->addScript(XOOPS_URL . '/browse.php?Frameworks/jquery/jquery.js');
+$GLOBALS['xoTheme']->addScript($helper->url('assets/js/jquery-ui-1.8.11.custom.js'));
+$GLOBALS['xoTheme']->addScript($helper->url('assets/js/smallworld_tabs.js'));
+$GLOBALS['xoTheme']->addScript('', '', $adminscript);
+$GLOBALS['xoTheme']->addScript($helper->url('assets/js/jquery.form.js'));
+$GLOBALS['xoTheme']->addScript($helper->url('assets/js/jquery.countdown.js'));
+if (file_exists($helper->path('language/' . $lang . '/js/jquery.ui.datepicker-language.js'))) {
+    $GLOBALS['xoTheme']->addScript($helper->url('language/' . $lang . '/js/jquery.ui.datepicker-language.js'));
+    $GLOBALS['xoTheme']->addScript($helper->url('language/' . $lang . '/js/jquery.countdown.js'));
 } else {
-    $xoTheme->addScript(XOOPS_URL . '/modules/smallworld/language/english/js/jquery.ui.datepicker-language.js');
-    $xoTheme->addScript(XOOPS_URL . '/modules/smallworld/language/english/js/jquery.countdown.js');
+    $GLOBALS['xoTheme']->addScript($helper->url('language/english/js/jquery.ui.datepicker-language.js'));
+    $GLOBALS['xoTheme']->addScript($helper->url('language/english/js/jquery.countdown.js'));
 }
-$xoTheme->addScript(XOOPS_URL . '/modules/smallworld/js/adminsmallworld.js');
+$GLOBALS['xoTheme']->addScript($helper->url('assets/js/adminsmallworld.js'));
 
 xoops_cp_footer();
