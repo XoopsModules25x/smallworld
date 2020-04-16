2122df8e22f4ae5e6113976e6aac99ee1dd8232a
zyspec <zyspec@yahoo.com>
2020-04-15 20:17:27 -0500

    Form class file minor bug fix
    * fixed radio() rendered HTML to make id attrib unique
    * removed extraneous </p> in dropdown_add() method rendering
    * minor code refactoring for maintainability
    * changed selected HTML for dropdown() method
    * added <label> for retrieveRadio() checkbox(es)
    * added seperator var for dropdown(), radio() & retrieveRadio() methods
    * use Constants interface constants
    * switch from global to $GLOBALS
    * update phpDoc comments

--- class/Form.php
+++ class/Form.php
@@ -15,70 +15,91 @@ namespace XoopsModules\Smallworld;
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\Smallworld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
+ */
+
+ /**
+ * Form Class
+ *
+ * creates HTML entities to display in forms
+ *
  */
 class Form
 {
     /**
-     * @create a dropdown select
+     * Create a dropdown select
+     *
      * @param string $name
      * @param array  $options
      * @param string $selected (optional)
+     * @param string $sep (optional)
      * @return string
      */
-    public function dropdown($name, array $options, $selected = null)
+    public function dropdown($name, array $options, $selected = null, $sep = '<br>')
     {
-        $dropdown = '<select name="' . $name . '" id="' . $name . '">' . '<br>';
-        $selected = $selected;
+        $dropdown = "<select name='{$name}' id='{$name}'>\n";
         foreach ($options as $key => $option) {
-            $select   = $selected == $key ? ' selected="yes"' : '';
-            $dropdown .= '<option value="' . $key . '"' . $select . '>' . $option . '</option>' . '<br>';
+            $select    = $selected == $key ? ' selected' : '';
+            $dropdown .= "<option value='{$key}'{$selected}>{$option}</option>\n";
         }
-        $dropdown .= '</select>' . '<br>';
+        $dropdown .= "</select>{$sep}";
 
         return $dropdown;
     }
 
     /**
-     * @create a radio select
+     * Create a radio select
+	 *
      * @param string $name
      * @param array  $options
      * @param string $selected (optional)
+     * @param string $sep (optional)
      * @return string
      */
-    public function radio($name, array $options, $selected = null)
+    public function radio($name, array $options, $selected = null, $sep = '<br>')
     {
         $selected = $selected;
         $form     = '';
+        $i = 0;
         foreach ($options as $value => $option) {
-            $select = $selected == $value ? ' checked' : ' checked="unchecked"';
-            $form   .= '<input type="checkbox" name="' . $name . '[]" id="' . $name . '" value="' . $value . '" ' . $select . '> ' . $option . '<br>';
+            $select = $selected == $value ? ' checked' : '';
+            $form   .= "<label for='{$name}-{$i}'><input type='radio' name='{$name}[]' id='{$name}-{$i}' value='{$value}'{$select}> "
+            . "{$option}</label>{$sep}\n";
+//            $form   .= "<input type='checkbox' name='{$name}[]' id='{$name}-{$i}' value='{$value}'{$select}> "
+//            . "<label for='{$name}-{$i}'>{$option}</label>{$sep}\n";
+            $i++;
         }
 
         return $form;
     }
 
     /**
+	 * Create a HTML checkbox element
+	 *
      * @param string $name
      * @param array  $options
      * @param array  $valuearray
      * @param string $selected (optional)
+     * @param string $sep (optional)
      * @return string
      */
-    public function retrieveRadio($name, $options, $valuearray, $selected = null)
+    public function retrieveRadio($name, $options, $valuearray, $selected = null, $sep = '<br>')
     {
         $form = '';
         $a    = count($options) - 1;
         for ($i = 0; $i <= $a; ++$i) {
             if (in_array($i, $valuearray)) {
-                $form .= '<input type="checkbox" id="' . $name . '-' . $i . '" name="' . $name . '[]" value="' . $i . '" checked>' . $options[$i] . '<br>';
+                $form .= "<input type='checkbox' id='{$name}-{$i}' name='{$name}[]' value='{$i}' checked> "
+                . "<label for='{$name}-{$i}'>{$options[$i]}</label>{$sep}";
             } else {
-                $form .= '<input type="checkbox" id="' . $name . '-' . $i . '" name="' . $name . '[]" value="' . $i . '" >' . $options[$i] . '<br>';
+                $form .= "<input type='checkbox' id='{$name}-{$i}' name='{$name}[]' value='{$i}'> "
+                . "<label for='{$name}-{$i}'>{$options[$i]}</label>{$sep}";
             }
         }
 
@@ -86,6 +107,8 @@ class Form
     }
 
     /**
+	 * Create a HTML text input element
+	 *
      * @param string $name
      * @param string $id
      * @param string $class
@@ -96,12 +119,14 @@ class Form
     public function input($name, $id, $class, $size = null, $preset = null)
     {
         $s    = $size ?: '50px';
-        $data = "<input type='text' size='" . $s . "' name='" . $name . "' id='" . $id . "' class='" . $class . "' value='" . $preset . "' >";
+        $data = "<input type='text' size='" . $s . "' name='" . $name . "' id='" . $id . "' class='" . $class . "' value='" . $preset . "'>";
 
         return $data;
     }
 
     /**
+	 * Create a HTML hidden element
+	 *
      * @param string $name
      * @param string $id
      * @param string $preset (optional)
@@ -115,6 +140,8 @@ class Form
     }
 
     /**
+	 * Returns simple text string
+	 *
      * @param string $value
      * @return string
      */
@@ -126,6 +153,8 @@ class Form
     }
 
     /**
+	 * Create a HTML select (dropdown) element
+	 *
      * @param string  $class
      * @param string  $name
      * @param         $name2
@@ -139,7 +168,7 @@ class Form
     public function dropdown_add($class, $name, $name2, $rel, array $options, $textmore, $selected = null, $preset = null)
     {
         $dropdown = '<span id="' . $name . '"><input type="text" name="' . $name2 . '[]" value="' . $preset . '">';
-        $dropdown .= '<select class="smallworld_select" name="' . $name . '[]" id="' . $name . '"></p>' . '<br>';
+        $dropdown .= '<select class="smallworld_select" name="' . $name . '[]" id="' . $name . '">' . '<br>';
         foreach ($options as $key => $option) {
             $select   = $selected == $key ? ' selected' : null;
             $dropdown .= '<option value="' . $key . '"' . $select . '>' . $option . '</option>' . '<br>';
@@ -158,7 +187,7 @@ class Form
      * @param string  $textmore
      * @param string  $preset (optional)
      * @param string  $id     (optional)
-     * @return dynamic|string
+     * @return string
      */
     public function input_add($class, $name, $name2, $rel, $size, $textmore, $preset = null, $id = null)
     {
@@ -177,7 +206,7 @@ class Form
      * @param string $cols
      * @param string $class
      * @param string $preset (optional)
-     * @return string|textarea
+     * @return string
      * @return string
      */
     public function textarea($name, $id, $title, $rows, $cols, $class, $preset = null)
@@ -196,7 +225,7 @@ class Form
      * @param string $preset        (optional)
      * @param string $selectedstart (optional)
      * @param string $selectedstop  (optional)
-     * @return dynamic|string
+     * @return string
      */
     public function school_add(
         $class,
@@ -210,23 +239,23 @@ class Form
         $selectedstart = null,
         $selectedstop = null
     ) {
-        $dropdown = '<div id="' . $name . '"><p class="smallworld_clonebreaker">' . _SMALLWORLD_SCHOOLNAME . '<input class="school" type="text" value="' . $preset . '" name="' . $name2 . '[]">';
-        $dropdown .= '<br><br>' . _SMALLWORLD_SCHOOLTYPE . '<select class="school" name="' . $name . '[]" id="' . $name . '"">' . '<br>';
+        $dropdown = '<div id="' . $name . '"><p class="smallworld_clonebreaker">' . _SMALLWORLD_SCHOOLNAME . '<input class="school" type="text" value="' . $preset . '" name="' . $name2 . '[]">'
+                  . '<br><br>' . _SMALLWORLD_SCHOOLTYPE . '<select class="school" name="' . $name . '[]" id="' . $name . '"">' . '<br>';
         foreach ($options as $key => $option) {
             $select   = $selected == $key ? ' selected="selected"' : null;
             $dropdown .= '<option  class="school" value="' . $key . '"' . $select . '>' . $option . '</option>' . '<br>';
         }
-        $dropdown .= '</select>';
-        $dropdown .= '<br><br>';
-        $dropdown .= _SMALLWORLD_START . '<select class="schooltime" name="schoolstart[]" id="schoolstart">';
+        $dropdown .= '</select>'
+                   . '<br><br>'
+                   . _SMALLWORLD_START . '<select class="schooltime" name="schoolstart[]" id="schoolstart">';
         $array    = SmallworldGetTimestampsToForm();
         foreach ($array as $key => $option) {
             $selectstart = $selectedstart == $key ? ' selected="selected"' : null;
             $dropdown    .= '<option value="' . $key . '"' . $selectstart . '>' . $option . '</option>' . '<br>';
         }
-        $dropdown .= '</select>';
-        $dropdown .= '<br><br>';
-        $dropdown .= _SMALLWORLD_STOP . '<select class="schooltime" name="schoolstop[]" id="schoolstop">';
+        $dropdown .= '</select>'
+                   . '<br><br>'
+                   . _SMALLWORLD_STOP . '<select class="schooltime" name="schoolstop[]" id="schoolstop">';
         $array    = SmallworldGetTimestampsToForm();
         foreach ($array as $key => $option) {
             $selectstop = $selectedstop == $key ? ' selected="selected"' : null;
@@ -238,17 +267,19 @@ class Form
     }
 
     /**
-     * @param string  $class
+     * Create HTML form elements for Job fields
+     *
+     * @param string  $class - not used
      * @param string  $name
-     * @param         $name2
-     * @param string  $rel
-     * @param string  $textmore
+     * @param string  $name2 - not used
+     * @param string  $rel   - not used
+     * @param string  $textmore - not used
      * @param string  $employer      (optional)
      * @param string  $position      (optional)
      * @param string  $selectedstart (optional)
      * @param string  $selectedstop  (optional)
      * @param string  $description   (optional)
-     * @return dynamic|string
+     * @return string
      */
     public function job(
         $class,
@@ -262,73 +293,71 @@ class Form
         $selectedstop = null,
         $description = null
     ) {
-        $text = '<div id="' . $name . '"><p class="smallworld_clonebreaker">' . _SMALLWORLD_EMPLOYER . '<input class="job" id="job" value="' . $employer . '" type="text" name="employer[]">';
-        $text .= '<br><br>' . _SMALLWORLD_POSITION . '<input class="job" type="text" value="' . $position . '" name="position[]">';
-        $text .= '<br><br>' . _SMALLWORLD_JOBSTART . '<input class="jobstart" type="text" value="' . $selectedstart . '" name="jobstart[]">';
-        $text .= '<br><br>' . _SMALLWORLD_JOBSTOP . '<input class="jobstop" value="' . $selectedstop . '" type="text" name="jobstop[]">';
-        $text .= '<br><br><span class="jobdescText">' . _SMALLWORLD_DESCRIPTION . '</span><textarea class="jobdesc" name="description[]" rows="20" cols="20">' . $description . '</textarea><br></p></div>';
-        $text .= '' . '<br>';
+        $text = '<div id="' . $name . '"><p class="smallworld_clonebreaker">' . _SMALLWORLD_EMPLOYER . '<input class="job" id="job" value="' . $employer . '" type="text" name="employer[]">'
+              . '<br><br>' . _SMALLWORLD_POSITION . '<input class="job" type="text" value="' . $position . '" name="position[]">'
+              . '<br><br>' . _SMALLWORLD_JOBSTART . '<input class="jobstart" type="text" value="' . $selectedstart . '" name="jobstart[]">'
+              . '<br><br>' . _SMALLWORLD_JOBSTOP . '<input class="jobstop" value="' . $selectedstop . '" type="text" name="jobstop[]">'
+              . '<br><br><span class="jobdescText">' . _SMALLWORLD_DESCRIPTION . '</span><textarea class="jobdesc" name="description[]" rows="20" cols="20">' . $description . '</textarea><br></p></div>'
+              . '' . '<br>';
 
         return $text;
     }
 
     /**
-     * @param int $userID
-     * @return file|string
+     * Create a HTML upload form
+     *
+     * @param int $userID - not used
      * @return string
      */
     public function uploadform($userID)
     {
-        $text = '<form action="../imgupload.php" method="POST" enctype="multipart/form-data">';
-        $text .= '<input type="file" name="file[]" multiple>';
-        $text .= '<button type="submit">' . _SMALLWORLD_UPLOADTEXT . '</button>';
-        $text .= '<span class="file_upload_label">' . _SMALLWORLD_UPLOADFILESTEXT . '</span>';
-        $text .= '</form>';
+        $text = '<form action="' . Helper::getInstance()->url('imgupload.php') . '" method="POST" enctype="multipart/form-data">'
+              . '<input type="file" name="file[]" multiple>'
+              . '<button type="submit">' . _SMALLWORLD_UPLOADTEXT . '</button>'
+              . '<span class="file_upload_label">' . _SMALLWORLD_UPLOADFILESTEXT . '</span>'
+              . '</form>';
 
         return $text;
     }
 
     /**
-     * @param int    $userID
+     * @param int    $userID - not used
      * @param string $imgurl
      * @param string $imgdesc
      * @param string $id
-     * @return dynamic|string
      * @return string
      */
     public function edit_images($userID, $imgurl, $imgdesc, $id)
     {
-        global $xoopsUser;
-        $text = '<p class="smallworld_clonebreaker"><br>';
-        $text .= '<table class="smallworld_table" border="0" cellspacing="0" cellpadding="0">';
-        $text .= '<tr>';
-        $text .= '<td><img class="smallworld_edit_image" src="' . $imgurl . '" height="100px" width="80px;"></td>';
-        $text .= '<td><span class="smallworld_editTextSpan">' . _SMALLWORLD_UPLOADDESC . '</span><br><br><textarea class="smallworld_edit_desc" name="imgdesc[]" rows="1" cols="1">' . stripslashes($imgdesc) . '</textarea><br><br></td>';
-        $text .= '<input value="' . $id . '" type="hidden" name="id[]"></p>';
-        $text .= '</tr></table>';
+        $text = '<p class="smallworld_clonebreaker"><br>'
+              . '<table class="smallworld_table" border="0" cellspacing="0" cellpadding="0">'
+              . '<tr>'
+              . '<td><img class="smallworld_edit_image" src="' . $imgurl . '" height="100px" width="80px;"></td>'
+              . '<td><span class="smallworld_editTextSpan">' . _SMALLWORLD_UPLOADDESC . '</span><br><br><textarea class="smallworld_edit_desc" name="imgdesc[]" rows="1" cols="1">' . stripslashes($imgdesc) . '</textarea><br><br></td>'
+              . '<input value="' . $id . '" type="hidden" name="id[]">'
+              . '</tr></table></p>';
 
         return $text;
     }
 
     /**
-     * @create a radio select
-     * @param $userid
+     * Create HTML radio selects for user settings
+     *
+     * @param int $userid
      * @return string
      */
     public function usersettings($userid)
     {
-        global $xoopsUser, $xoopsDB;
-
         $form = "<div style='display:none'><div class='smallworld_usersetings'>";
         $form .= '<fieldset><legend>' . _SMALLWORLD_SHOWIFPUBLICORPRIVATE . '</legend>';
         $form .= "<form id='perset'>";
-        if ($xoopsUser) {
-            $sql    = 'SELECT value FROM ' . $xoopsDB->prefix('smallworld_settings') . ' WHERE userid = ' . (int)$userid;
-            $result = $xoopsDB->queryF($sql);
-            $i      = $xoopsDB->getRowsNum($result);
+        if ($GLOBALS['xoopsUser'] && $GLOBALS['xoopsUser'] instanceof \XoopsUser) {
+            $sql    = 'SELECT value FROM ' . $GLOBALS['xoopsDB']->prefix('smallworld_settings') . ' WHERE userid = ' . (int)$userid;
+            $result = $GLOBALS['xoopsDB']->queryF($sql);
+            $i      = $GLOBALS['xoopsDB']->getRowsNum($result);
             $v      = [];
             if ($i >= 1) {
-                while (false !== ($row = $xoopsDB->fetchArray($result))) {
+                while (false !== ($row = $GLOBALS['xoopsDB']->fetchArray($result))) {
                     $v    = unserialize(stripslashes($row['value']));
                     $pv   = ('1' == $v['posts']) ? ' checked' : '';
                     $cv   = ('1' == $v['comments']) ? ' checked' : '';
