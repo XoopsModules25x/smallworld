12980d784ffe51d611861f961c8c2872a0ff49e6
zyspec <zyspec@yahoo.com>
2020-04-15 19:49:47 -0500

    minor updates for loadmore.php
    * minor code refactoring for maintainability
    * use Xmf\Request to sanitize input vars
    * combine tpl assigns to improve performance
    * use Constants interface constants
    * update phpDoc comments

--- loadmore.php
+++ loadmore.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -12,23 +12,27 @@
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\Smallworld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
  */
 
+use Xmf\Request;
 use XoopsModules\Smallworld;
+use XoopsModules\Smallworld\Constants;
 
 require_once __DIR__ . '/header.php';
 
 require_once XOOPS_ROOT_PATH . '/class/template.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/functions.php';
-//require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/class_collector.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/arrays.php';
-//require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/WallUpdates.php';
+
+/** @var \XoopsModules\Smallworld\Helper $helper */
+require_once $helper->path('include/functions.php');
+require_once $helper->path('include/arrays.php');
+
 $set = smallworld_checkPrivateOrPublic();
 $pub = smallworld_checkUserPubPostPerm();
 $hm  = smallworld_GetModuleOption('msgtoshow');
@@ -36,64 +40,71 @@ $hm  = smallworld_GetModuleOption('msgtoshow');
 $last = $GLOBALS['xoopsDB']->escape($_POST['last']);
 $page = $GLOBALS['xoopsDB']->escape($_POST['page']);
 
-global $xoopsUser, $xoTheme, $xoopsTpl, $xoopsLogger;
-$xoopsLogger->activated = false;
+$GLOBALS['xoopsLogger']->activated = false;
 /* error_reporting(E_ALL); */
-$xoopsTpl = new \XoopsTpl();
-$id       = $xoopsUser ? $xoopsUser->getVar('uid') : 0;
-if ($id <= 0 || 'publicindex' === $page && $set['access'] = 1) {
+
+//$GLOBALS['xoopsTpl'] = new \XoopsTpl();
+$check    = new Smallworld\User();
+$id       = Constants::DEFAULT_UID;
+$username = '';
+$profile  = Constants::PROFILE_NONE;
+$isAdmin  = $helper->isUserAdmin();
+$tplAdmin= $isAdmin ? 'YES' : 'NO';
+
+if ($GLOBALS['xoopsUser'] && ($GLOBALS['xoopsUser'] instanceof \XoopsUser)) {
+    $id       = $GLOBALS['xoopsUser']->uid();
+    $username = $GLOBALS['xoopsUser']->uname();
+    $profile  = $check->checkIfProfile($id);
+    if ($isAdmin) {
+        $pub = $check->allUsers();
+    }
+}
+
+$userid = Request::getInt('userid', $id, 'POST');
+if (Constants::DEFAULT_UID < $userid && $userid !== $id) {
+    $xUser = new \XoopsUser($userid);
+    if ($xUser && ($xUser instanceof \XoopsUser)) {
+        $id       = $userid;
+        $username = $xUser->uname();
+        $profile  = $check->checkIfProfile($id);
+    }
+}
+
+if (Constants::DEFAULT_UID >= $id || ('publicindex' === $page) && (Constants::HAS_ACCESS == $set['access'])) {
     $wall = new Smallworld\PublicWallUpdates();
 } else {
     $wall = new Smallworld\WallUpdates();
 }
-if (isset($_POST['userid'])) {
-    $userid = (int)$_POST['userid'];
-} else {
-    $userid = $xoopsUser ? $xoopsUser->getVar('uid') : 0;
-}
-$Xuser    = ($id > 0) ? new \XoopsUser($id) : 0;
-$username = ($id > 0) ? $Xuser->getVar('uname') : '';
-$dBase    = new Smallworld\SwDatabase();
-$check    = new Smallworld\User();
-$profile  = $xoopsUser ? $check->checkIfProfile($id) : 0;
 
-if ($id > 0) {
-    if ($xoopsUser->isAdmin($xoopsModule->getVar('mid'))) {
-        $pub = $check->allUsers();
-        $xoopsTpl->assign('isadminuser', 'YES');
-    }
-} else {
-    $xoopsTpl->assign('isadminuser', 'NO');
-    $pub = smallworld_checkUserPubPostPerm();
-}
-
-if ($id <= 0 && 1 == $set['access']) {
-    //$pub = $check->allUsers();
-    $followers = $pub;
-} elseif ($id > 0 && 1 == $set['access'] && 'publicindex' === $page) {
-    //$pub = $check->allUsers();
+if (Constants::DEFAULT_UID >= $id && Constants::HAS_ACCESS == $set['access']) {
     $followers = $pub;
 } else {
     $followers = smallworld_array_flatten($wall->getFollowers($id), 0);
 }
 
-if ('index' === $page) {
-    $updatesarray = ($id > 0) ? $wall->Updates($_POST['last'], $id, $followers) : $wall->Updates($_POST['last'], $followers);
-} elseif ('profile' === $page) {
-    $updatesarray = ($id > 0) ? $wall->Updates($_POST['last'], $userid, $userid) : $wall->Updates($_POST['last'], $userid);
-} elseif ('publicindex' === $page) {
-    $updatesarray = $wall->Updates($_POST['last'], $followers);
+$last = Request::getString('last', 'a', 'POST');
+switch ($page) {
+    case ('index'):
+        $updatesArray = (Constants::DEFAULT_UID < $id) ? $wall->Updates($last, $id, $followers) : $wall->Updates($last, $followers);
+        break;
+    case ('profile'):
+        $updatesArray = (Constants::DEFAULT_UID < $id) ? $wall->Updates($last, $userid, $userid) : $wall->Updates($last, $userid);
+        break;
+    case ('publicindex'):
+        $updatesArray = $wall->Updates($last, $followers);
+        break;
 }
 
-$wall->parsePubArray($updatesarray, $id);
+$wall->parsePubArray($updatesArray, $id);
 
-$xoopsTpl->assign('sCountResp', count($updatesarray));
-$xoopsTpl->assign('msgtoshow', $hm);
-$xoopsTpl->assign('myusername', $username);
-$xoopsTpl->assign('pagename', $page);
+$GLOBALS['xoopsTpl']->assign([
+    'sCountResp'  => count($updatesArray),
+    'msgtoshow'   => $hm,
+    'myusername'  => $username,
+    'pagename'    => $page,
+    'isadminuser' => $tplAdmin
+]);
 
-if ($id > 0) {
-    $xoopsTpl->display(XOOPS_ROOT_PATH . '/modules/smallworld/templates/getmore.tpl');
-} else {
-    $xoopsTpl->display(XOOPS_ROOT_PATH . '/modules/smallworld/templates/getmorepublic.tpl');
-}
+$tplFileName = (Constants::DEFAULT_UID < $id) ? $helper->path('templates/getmore.tpl') : $helper->path('templates/getmorepublic.tpl');
+$GLOBALS['xoopsTpl']->display($tplFileName);
+
