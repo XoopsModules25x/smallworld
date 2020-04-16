f78d5a11d62aa706f6980ea756aaf5926147fed4
zyspec <zyspec@yahoo.com>
2020-04-15 19:51:45 -0500

    minor updates to img_upload.php
    * use Constants interface constants
    * update phpDoc comments

--- img_upload.php
+++ img_upload.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -12,44 +12,46 @@
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\Smallworld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
  */
 
 use XoopsModules\Smallworld;
+use XoopsModules\Smallworld\Constants;
 
 require_once __DIR__ . '/header.php';
 
+/** @var \XoopsModules\Smallworld\Helper $helper */
 $GLOBALS['xoopsOption']['template_main'] = 'smallworld_userprofile_imgupload.tpl';
 require_once XOOPS_ROOT_PATH . '/header.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/functions.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/arrays.php';
-//require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/class_collector.php';
 
-if ($xoopsUser) {
-    global $xoTheme;
-    $xoopsLogger->activated = false;
+require_once $helper->path('include/functions.php');
+require_once $helper->path('include/arrays.php');
 
-    $id      = $xoopsUser->getVar('uid');
+if ($GLOBALS['xoopsUser'] && ($GLOBALS['xoopsUser'] instanceof \XoopsUser)) {
+    $GLOBALS['xoopsLogger']->activated = false;
+
+    $id      = $GLOBALS['xoopsUser']->getVar('uid');
     $check   = new Smallworld\User();
     $profile = $check->checkIfProfile($id);
-    if ($profile >= 2) {
-        $xoopsTpl->assign('check', $profile);
+    if ($profile >= Constants::PROFILE_HAS_BOTH) {
+        $GLOBALS['xoopsTpl']->assign('check', $profile);
         $item = new Smallworld\Form();
-        $db   = new Smallworld\SwDatabase();
+        //$swDB = new Smallworld\SwDatabase();
 
         // ------------ DISPLAY IMAGES ------------ //
         // ------------ IMAGE UPLOADER ------------ //
         // Image upload form
         $upload = $item->uploadform($id);
     } else {
-        redirect_header('register.php', 1);
+        $helper->redirect('register.php', Constants::REDIRECT_DELAY_SHORT);
     }
 } else {
-    redirect_header(XOOPS_URL . '/user.php', 1, _NOPERM);
+    redirect_header(XOOPS_URL . '/user.php', Constants::REDIRECT_DELAY_SHORT, _NOPERM);
 }
 require_once XOOPS_ROOT_PATH . '/footer.php';
