b952e7f0873eff42c7105c6a521d0203dd6476ac
Michael Albertsen <culex@culex.dk>
2020-12-18 13:53:16 +0100

    Update header.php
    Changes by Mamba / Zyspec

--- header.php
+++ header.php
@@ -1,16 +1,27 @@
 <?php
 
-use Xoopsmodules\smallworld;
+use XoopsModules\Smallworld;
 
+include __DIR__ . '/preloads/autoloader.php';
 require_once dirname(dirname(__DIR__)) . '/mainfile.php';
+
 $moduleDirName = basename(__DIR__);
+$modulePath    = XOOPS_ROOT_PATH . '/modules/' . $moduleDirName;
 
-//xoops_load('instruction', $moduleDirName);
+//require XOOPS_ROOT_PATH . '/header.php';
 
-//
-//include_once __DIR__ . '/class/utility.php';
-require_once __DIR__ . '/include/common.php';
+$helper = Smallworld\Helper::getInstance();
+// Load language files
+$helper->loadLanguage('main');
 
-//$helper = new \Xoopsmodules\instruction\Helper::getHelper($moduleDirName);
-// Трей
+$myts   = \MyTextSanitizer::getInstance();
 
+if (!isset($GLOBALS['xoTheme']) || !$GLOBALS['xoTheme'] instanceof \xos_opal_Theme) {
+    require $GLOBALS['xoops']->path('class/theme.php');
+    $GLOBALS['xoTheme'] = new \xos_opal_Theme();
+}
+
+if (!isset($GLOBALS['xoopsTpl']) || !$GLOBALS['xoopsTpl'] instanceof \XoopsTpl) {
+    require $GLOBALS['xoops']->path('class/template.php');
+    $GLOBALS['xoopsTpl'] = new \XoopsTpl();
+}
