0f4be7f81a6986d54d416dd831c0a606fd852433
Michael Albertsen <culex@culex.dk>
2020-12-18 13:57:08 +0100

    changes
    changes by Mamba / Zyspec / Culex

--- publicindex.php
+++ publicindex.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -12,81 +12,82 @@
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\SmallWorld
  * @copyright    The XOOPS Project (https://xoops.org)
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
  */
 
-use Xmf\Request;
-use Xoopsmodules\smallworld;
+use XoopsModules\Smallworld;
+use XoopsModules\Smallworld\Constants;
+
 require_once __DIR__ . '/header.php';
 
-require_once __DIR__ . '/../../mainfile.php';
-$page = basename($_SERVER['PHP_SELF'], '.php');
+$page = basename(__FILE__, '.php');
 
-if ($xoopsUser && 'publicindex' !== $page) {
+// @ todo - figure out why the test for $page is here... $page will always equal 'publicindex'
+if ($GLOBALS['xoopsUser'] && ($GLOBALS['xoopsUser'] instanceof \XoopsUser) && 'publicindex' !== $page) {
     $GLOBALS['xoopsOption']['template_main'] = 'smallworld_index.tpl';
 } else {
     $GLOBALS['xoopsOption']['template_main'] = 'smallworld_publicindex.tpl';
 }
 require_once XOOPS_ROOT_PATH . '/header.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/class_collector.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/PublicWallUpdates.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/functions.php';
-global $xoopsUser, $xoTheme, $xoopsConfig, $xoopsLogger;
-//$xoopsLogger->activated = true;
+
+/** @var \XoopsModules\Smallworld\Helper $helper */
+require_once $helper->path('include/functions.php');
+//$GLOBALS['xoopsLogger']->activated = true;
 //error_reporting(E_ALL);
 
-$set   = smallworld_checkPrivateOrPublic();
-$dBase = new smallworld\SmallWorldDB;
-$check = new smallworld\SmallWorldUser;
+$set      = smallworld_checkPrivateOrPublic();
+$swDB     = new Smallworld\SwDatabase();
+$id       = Constants::DEFAULT_UID;
+$username = '';
+$profile  = Constants::PROFILE_NONE;
 
-$id       = $xoopsUser ? $xoopsUser->getVar('uid') : 0;
-$username = $xoopsUser ? $xoopsUser->getVar('uname') : '';
-$profile  = $xoopsUser ? $check->CheckIfProfile($id) : 0;
+if ($GLOBALS['xoopsUser'] instanceof \XoopsUser) {
+    $id       = $GLOBALS['xoopsUser']->uid();
+    $username = $GLOBALS['xoopsUser']->uname();
+    //$check    = new Smallworld\User();
+    //$profile  = $check->checkIfProfile($id);
+    $profile  = $helper->getHandler('SwUser')->checkIfProfile($id);
+}
 
-$moduleHandler = xoops_getHandler('module');
-$module        = $moduleHandler->getByDirname('smallworld');
-$configHandler = xoops_getHandler('config');
-$moduleConfig  = $configHandler->getConfigsByCat(0, $module->getVar('mid'));
-$pub            = smallworld_checkUserPubPostPerm();
-$wall           = new smallworld\PublicWallUpdates;
-$updates        = $wall->Updates(0, $pub);
+$pub     = smallworld_checkUserPubPostPerm();
+$wall    = new Smallworld\PublicWallUpdates();
+$updates = $wall->Updates(0, $pub);
 
 if ($id > 0) {
-    $xoopsTpl->assign('ownerofpage', $id);
-    $xoopsTpl->assign('myusername', $username);
+    $GLOBALS['xoopsTpl']->assign('ownerofpage', $id);
+    $GLOBALS['xoopsTpl']->assign('myusername', $username);
 }
 
-if ($xoopsUser) {
-    if ($xoopsUser->isAdmin($xoopsModule->getVar('mid'))) {
-        $pub = $check->allUsers();
-        $xoopsTpl->assign('isadminuser', 'YES');
-    }
-} else {
-    $xoopsTpl->assign('isadminuser', 'NO');
-}
+$tplAdmin = $helper->isUserAdmin() ? 'YES' : 'NO';
 
 // Create form for private settings
-$form         = new smallworld\SmallWorldForm;
+$form         = new Smallworld\Form();
 $usersettings = $form->usersettings($id, $selected = null);
-$xoopsTpl->assign('usersetting', $usersettings);
+$GLOBALS['xoopsTpl']->assign('usersetting', $usersettings);
 
-$xuser = new smallworld\SmallWorldProfile;
+//$xuser = new Smallworld\Profile();
 
-$menu_home     = "<a href='" . XOOPS_URL . "/modules/smallworld/'><img id='menuimg' src='" . XOOPS_URL . "/modules/smallworld/assets/images/house.png'>" . _SMALLWORLD_HOME . '</a>';
-$menu_register = ($profile < 2) ? "<a href='" . XOOPS_URL . "/modules/smallworld/register.php'><img id='menuimg' src='" . XOOPS_URL . "/modules/smallworld/assets/images/join.jpg'>" . _MB_SYSTEM_RNOW . '</a>' : '';
+$menu_home     = "<a href='" . $helper->url('/') . "'><img id='menuimg' src='" . $helper->url('assets/images/house.png') . "'>" . _SMALLWORLD_HOME . '</a>';
+$menu_register = ($profile < Constants::PROFILE_HAS_BOTH) ? "<a href='" . $helper->url('register.php') . "'><img id='menuimg' src='" . $helper->url('assets/images/join.jpg') . "'>" . _MB_SYSTEM_RNOW . '</a>' : '';
 
 $updatesarray = $wall->Updates(0, $pub);
-$wall->ParsePubArray($updatesarray, $id);
+$wall->parsePubArray($updatesarray, $id);
 
-$xoopsTpl->assign('menu_home', $menu_home);
-$xoopsTpl->assign('menu_register', $menu_register);
-$xoopsTpl->assign('pagename', 'publicindex');
-$xoopsTpl->assign('check', $profile);
-$xoopsTpl->assign('access', $set['access']);
+$GLOBALS['xoopsTpl']->assign(
+    [
+        'menu_home'     => $menu_home,
+        'menu_register' => $menu_register,
+        'pagename'      => 'publicindex',
+        'check'         => $profile,
+        'access'        => $set['access'],
+        'isadminuser'   => $tplAdmin
+    ]
+);
 
 require_once XOOPS_ROOT_PATH . '/footer.php';
--- stats.php
+++ stats.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -12,46 +12,67 @@
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\Smallworld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
  */
 
-use Xmf\Request;
-use Xoopsmodules\smallworld;
+use XoopsModules\Smallworld;
+
 require_once __DIR__ . '/header.php';
 
-require_once __DIR__ . '/../../mainfile.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/functions.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/include/arrays.php';
-require_once XOOPS_ROOT_PATH . '/modules/smallworld/class/class_collector.php';
+/** @var \XoopsModules\Smallworld\Helper $helper */
+require_once $helper->path('include/functions.php');
+require_once $helper->path('include/arrays.php');
 require_once XOOPS_ROOT_PATH . '/class/template.php';
-global $xoopsUser, $xoopsTpl, $xoTheme;
-$xoopsLogger->activated = false;
-//error_reporting(E_ALL);
-if ($xoopsUser) {
-    $tpl = new XoopsTpl();
 
-    $userid    = $xoopsUser->getVar('uid');
-    $newusers  = smallworld_Stats_newest();
-    $m_a_users = Smallworld_mostactiveusers_allround();
-    $br_users  = Smallworld_topratedusers();
-    $wo_users  = Smallworld_worstratedusers();
-    $birth     = smallworld_nextBirthdays();
-    $sp        = smallworld_sp();
-    $tpl->assign('newusers', $newusers);
-    $tpl->assign('mostactiveU', $m_a_users);
-    $tpl->assign('bestratedU', $br_users);
-    $tpl->assign('worstratedU', $wo_users);
-    $tpl->assign('sp', $sp);
-    //readfile(XOOPS_ROOT_PATH .'/modules/smallworld/templates/getStat.html');
-    if (!empty($birth)) {
-        $tpl->assign('birthdays', $birth);
-    } else {
-        $tpl->assign('birthdays', 0);
+$GLOBALS['xoopsLogger']->activated = false;
+//error_reporting(E_ALL);
+if ($GLOBALS['xoopsUser'] instanceof \XoopsUser) {
+    $tpl = new \XoopsTpl();
+
+    //$userid    = $GLOBALS['xoopsUser']->uid();
+    //$newUsers  = smallworld_Stats_newest();
+/*************************************/
+    $newUsers        = [];
+    $swUserHandler   = $helper->getHandler('SwUser');
+    $criteria        = new \Criteria('');
+    $criteria->setSort('regdate');
+    $criteria->setLimit(3);
+    $criteria->order = 'DESC';
+    $swUserArray     = $swUserHandler->getAll($criteria, null, false);
+    foreach($swUserArray as $swUser) {
+        //$regdate    = date('d-m-Y', $swUser['regdate']);
+        $regdate    = date(_SHORTDATESTRING, $swUser['regdate']);
+        $newUsers[] = [
+            'userid'         => $swUser['userid'],
+            'username'       => $swUser['username'],
+            'regdate'        => $regdate,
+            'username_link'  => "<a href='" . $helper->url('userprofile.php?username=' . $swUser['username']) . "'>"
+                             . "{$swUser['username']} ({$swUser['realname']}) [{$regdate}]</a>",
+            'userimage'      => $swUser['userimage'],
+            'userimage_link' => $swUserHandler->getAvatarLink($swUser['userid'], $swUserHandler->gravatar($swUser['userid']))
+        ];
     }
-    $tpl->display(XOOPS_ROOT_PATH . '/modules/smallworld/templates/getStat.tpl');
+/*************************************/
+    $m_a_users = smallworld_mostactiveusers_allround();
+    $br_users  = smallworld_topratedusers();
+    $wo_users  = smallworld_worstratedusers();
+    $birth     = smallworld_nextBirthdays();
+    $bday      = !empty($birth) ? $birth : 0;
+    $sp        = smallworld_sp();
+    $tpl->assign([
+        'newusers'    => $newUsers,
+        'mostactiveU' => $m_a_users,
+        'bestratedU'  => $br_users,
+        'worstratedU' => $wo_users,
+        'sp'          => $sp,
+        'birthdays'   => $bday
+    ]);
+    //readfile(XOOPS_ROOT_PATH .'/modules/smallworld/templates/getStat.tpl');
+    $tpl->display($helper->path('templates/getStat.tpl'));
 }
