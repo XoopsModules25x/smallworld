21e3da8aa0fa9e8fc87becd3f6a5b3e71cb1a856
Michael Albertsen <culex@culex.dk>
2020-12-18 13:28:17 +0100

    Update images.php
    - namespaces
    - descriptions
    - using new globals

--- class/images.php
+++ class/images.php
@@ -1,5 +1,8 @@
 <?php
-/**
+
+namespace XoopsModules\Smallworld;
+
+/*
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -12,81 +15,99 @@
 /**
  * SmallWorld
  *
+ * @package      \XoopsModules\Smallworld
+ * @license      GNU GPL (https://www.gnu.org/licenses/gpl-2.0.html/)
  * @copyright    The XOOPS Project (https://xoops.org)
  * @copyright    2011 Culex
- * @license      GNU GPL (http://www.gnu.org/licenses/gpl-2.0.html/)
- * @package      SmallWorld
- * @since        1.0
  * @author       Michael Albertsen (http://culex.dk) <culex@culex.dk>
+ * @link         https://github.com/XoopsModules25x/smallworld
+ * @since        1.0
  */
-class SmallWorldImages
+
+/**
+ *
+ * Images Class
+ *
+ * Handles album creation and retrieval
+ *
+ */
+class Images
 {
     /**
-     * @Create folders
-     * @param  int $userID
+     * Create album folders
+	 *
+	 * @todo use \Smallworld\SysUtility::createFolder() to refactor this method
+     * @param int $userId
      * @return void
      */
-    public function createAlbum($userID)
+    public function createAlbum($userId)
     {
-        $dir = XOOPS_ROOT_PATH . '/uploads/albums_smallworld';
-        if (!file_exists($dir . '/' . $userID . '/thumbnails') || !file_exists($dir . '/' . $userID . '/')) {
+        //@todo change $dir to XOOPS_UPLOAD_PATH . /smallworld/albums/[$userId]
+        $dir = XOOPS_UPLOAD_PATH . '/albums_smallworld';
+        if (!file_exists($dir . '/' . $userId . '/thumbnails') || !file_exists($dir . '/' . $userId . '/')) {
             if (!is_dir($dir . '/')) {
-                mkdir($dir, 0777);
+                if (!mkdir($dir, 0777) && !is_dir($dir)) {
+                    throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
+                }
             } else {
-                mkdir($dir . '/' . $userID, 0777);
-                mkdir($dir . '/' . $userID . '/thumbnails', 0777);
-                Smallworld_CreateIndexFiles($dir . '/');
-                Smallworld_CreateIndexFiles($dir . '/' . $userID . '/');
-                Smallworld_CreateIndexFiles($dir . '/' . $userID . '/thumbnails/');
+                if (!mkdir($concurrentDirectory = $dir . '/' . $userId, 0777) && !is_dir($concurrentDirectory)) {
+                    throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
+                }
+                if (!mkdir($concurrentDirectory = $dir . '/' . $userId . '/thumbnails', 0777) && !is_dir($concurrentDirectory)) {
+                    throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
+                }
+                file_put_contents("{$dir}/index.html", '<script>history.go(-1);</script>');
+                file_put_contents("{$dir}/{$userId}/index.html", '<script>history.go(-1);</script>');
+                file_put_contents("{$dir}/{$userId}/thumbnails/index.html", '<script>history.go(-1);</script>');
             }
         }
     }
 
     /**
-     * @View user album. Userid = owner, user = visitor
-     * @param  int $userID
-     * @param  int $user
+     * View user album
+     *
+     * @param int $userId owner
+     * @param int $user visitors
      * @return array|bool
      */
-    public function viewalbum($userID, $user)
+    public function viewalbum($userId, $user)
     {
-        global $xoopsUser, $xoopsDB, $xoopsTpl;
         $post        = [];
-        $checkFriend = new SmallWorldUser;
-        if (0 != $checkFriend->friendcheck($userID, $user)) {
+        $checkFriend = new User();
+        $foundArray = $checkFriend->friendcheck((int)$userId, (int)$user);
+        if (0 < count($foundArray)) {
             // check friend is good to go
-            $sql    = 'SELECT * FROM ' . $xoopsDB->prefix('smallworld_images') . " WHERE userid = '" . $user . "'";
-            $result = $xoopsDB->query($sql);
-            $i      = 0;
-            while ($sqlfetch = $xoopsDB->fetchArray($result)) {
-                $post[$i]['id']      = stripslashes($sqlfetch['id']);
-                $post[$i]['userid']  = stripslashes($sqlfetch['userid']);
-                $post[$i]['imgurl']  = stripslashes($sqlfetch['imgurl']);
-                $post[$i]['desc']    = Smallworld_cleanup_string($sqlfetch['desc']);
-                $post[$i]['alt']     = Smallworld_cleanup_string($sqlfetch['desc']);
-                $post[$i]['time']    = stripslashes($sqlfetch['time']);
-                $post[$i]['editimg'] = "<span class='smallworld_edit_imgdesc_holder'><img src='assets/images/edit_icon.png'></span> <a class='smallworld_edit_imgdesc' href='editimages.php'>" . _SMALLWORLD_EDITDESCRIPTION . '</a>';
-                ++$i;
+            $sql    = 'SELECT * FROM ' . $GLOBALS['xoopsDB']->prefix('smallworld_images') . " WHERE userid = '" . (int)$user . "'";
+            $result = $GLOBALS['xoopsDB']->query($sql);
+            while (false !== ($sqlfetch = $GLOBALS['xoopsDB']->fetchArray($result))) {
+                $post[] = [
+                    'id'      => stripslashes($sqlfetch['id']),
+                    'userid'  => stripslashes($sqlfetch['userid']),
+                    'imgurl'  => stripslashes($sqlfetch['imgurl']),
+                    'desc'    => SwFunc\cleanupString($sqlfetch['desc']),
+                    'alt'     => SwFunc\cleanupString($sqlfetch['desc']),
+                    'time'    => stripslashes($sqlfetch['time']),
+                    'editimg' => "<span class='smallworld_edit_imgdesc_holder'><img src='" . Helper::getInstance()->url('images/edit_icon.png') . "'></span> <a class='smallworld_edit_imgdesc' href='editimages.php'>" . _SMALLWORLD_EDITDESCRIPTION . '</a>'
+                ];
             }
 
             return $post;
-        } else {
-            //Not a friends album
-            return false;
         }
+        //Not a friends album
+        return false;
     }
 
     /**
-     * @Get image count for user
-     * @param  int $userid
+     * Get image count for user
+     *
+     * @param int $userid
      * @return int
      */
     public function count($userid)
     {
-        global $xoopsDB;
-        $sql    = 'SELECT * FROM ' . $xoopsDB->prefix('smallworld_images') . " WHERE userid = '" . $userid . "'";
-        $result = $xoopsDB->queryF($sql);
+        $sql    = 'SELECT * FROM ' . $GLOBALS['xoopsDB']->prefix('smallworld_images') . " WHERE userid = '" . $userid . "'";
+        $result = $GLOBALS['xoopsDB']->queryF($sql);
 
-        return $xoopsDB->getRowsNum($result);
+        return $GLOBALS['xoopsDB']->getRowsNum($result);
     }
 }
